98d5ef2aa159bb42be2d1e424193feea46b9f426
JessCodes <jessicafellison@gmail.com>
2017-03-28 13:05:35 -0700

    update controllers with json

--- app/controllers/api/films_controller.rb
+++ app/controllers/api/films_controller.rb
@@ -1,11 +1,11 @@
 class Api::FilmsController < ApplicationController
   def index
     @films = Film.all
-    render :index
+    format.json { render json: @films }
   end
 
   def show
     @film = Film.find_by_id(params[:id])
-    render :show
+    format.json { render json: @film }
   end
 end
--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -2,25 +2,25 @@ class Api::RatingsController < ApplicationController
   def create
     @rating = Rating.new(rating_params)
     if @rating.save
-      render :show
+      format.json { render json: @rating }
     else
       render status: 422
     end
   end
 
-  def update
-    if @rating.update(rating_params)
-      render :show
-    else
-      render status: 422
-    end
-  end
-
-
-  # def destroy
-  #   @rating = Rating.find(params[:id])
-  #   @rating.destroy
+  # def update
+  #   if @rating.update(rating_params)
+  #     render :show
+  #   else
+  #     render status: 422
+  #   end
   # end
+
+
+  def destroy
+    @rating = Rating.find(params[:id])
+    @rating.destroy
+  end
 
   private
   def rating_params
